146c918fed7864f185f1a3245d61a6acb4b323e7
Sehgel <sehgel.dev@gmail.com>
2024-12-22 17:56:58 -0300

    Implement head texture

--- shaders/tail/tail_fragment.glsl
+++ shaders/tail/tail_fragment.glsl
@@ -1,10 +1,12 @@
 #version 330 core
+in vec2 uv;
 
 out vec4 out_color;
 
 uniform vec3 tint;
+uniform sampler2D text;
 
 void main()
 {
-    out_color = vec4(tint,1.0);
+    out_color = texture(text,uv) * vec4(tint,1.0);
 }
--- shaders/tail/tail_vertex.glsl
+++ shaders/tail/tail_vertex.glsl
@@ -1,5 +1,8 @@
 #version 330 core
 layout (location = 0) in vec2 in_pos;
+layout (location = 1) in vec2 in_uv;
+
+out vec2 uv;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -8,4 +11,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(in_pos,0.0, 1.0);
+    uv = in_uv;
 }
